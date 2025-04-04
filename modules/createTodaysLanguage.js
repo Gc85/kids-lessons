c4e12fcfcc99b25e96b2677575161f393e1c4163
nova-gavin <gavin_cronje@nova.co.jp>
2025-04-04 19:23:50 +0900

    Todays Language and Vocab modules

---
+++ modules/createTodaysLanguage.js
@@ -0,0 +1,27 @@
+import { createElem } from "./utils.js";
+import { createHeading } from "./createHeading.js";
+
+export function createTodaysLanguageSection(lesson, book, level) {
+
+  const todayLangDivElem = createElem('div', 'div-opening', '');
+  const todayLangH2Elm = createHeading(lesson, `Today's Language`, '1', '1');
+  const todayLangImgDiv = createElem('div', 'flex-div', '');
+
+  const todayLangTextElem = createElem('p', 'main-text', '');
+  todayLangTextElem.innerHTML = todayLangMessage;
+
+  const todayLangImgElem = createElem('img', 'image-big', '');
+  todayLangImgElem.src = `./images/${book}/${level}/${lesson.todayLangImage}.jpg`;
+
+  todayLangImgDiv.appendChild(todayLangImgElem);
+
+  todayLangDivElem.appendChild(todayLangH2Elm);
+  todayLangDivElem.appendChild(todayLangTextElem);
+  todayLangDivElem.appendChild(todayLangImgDiv);
+
+  return todayLangDivElem;
+}
+
+const todayLangMessage = [
+  `<b>Aim:</b> <em>Present target language in context.</em>`
+]
---
+++ modules/createVocabulary.js
@@ -0,0 +1,41 @@
+import { createElem } from "./utils.js";
+import { createHeading } from "./createHeading.js";
+
+export function createTodaysVocabularySection(lesson, book, level) {
+
+  const vocabDivElem = createElem('div', 'div-opening', '');
+  const vocabH2Elm = createHeading(lesson, `Vocabulary`, '2', '2');
+  const vocabImgDiv = createElem('div', 'flex-div flex-wrap flex-justify-evenly', '');
+
+  const vocabTextElem = createElem('p', 'main-text', '');
+  vocabTextElem.innerHTML = vocabMessage;
+
+  lesson.vocabImages.forEach((vocabItem) => {
+
+    const vocabImgElem = createElem('img', 'image-small', '');
+    vocabImgElem.src = `./images/${book}/${level}/${vocabItem}.jpg`;
+
+    vocabImgDiv.appendChild(vocabImgElem);
+  });
+
+  const vocabAudioPElem = createElem('p', 'audio', '');
+  const vocabAudioElem = createElem('audio', '', '');
+  vocabAudioElem.src = `./images/${book}/${level}/${lesson.vocabAudio}.mp3`;
+  vocabAudioPElem.innerHTML = vocabAudioMessage;
+  vocabAudioPElem.appendChild(vocabAudioElem);
+
+  vocabDivElem.appendChild(vocabH2Elm);
+  vocabDivElem.appendChild(vocabTextElem);
+  vocabDivElem.appendChild(vocabImgDiv);
+  vocabDivElem.appendChild(vocabAudioPElem);
+
+  return vocabDivElem;
+}
+
+const vocabMessage = [
+  `<b>Aim:</b> <em>Present flashcards and practice saying the target language.</em>`
+]
+
+const vocabAudioMessage = [
+  `<em>Play audio and sing along with the students.</em>`
+]
