025dff443f6a54e9feadf46ff40c35624b39d616
nova-gavin <gavin_cronje@nova.co.jp>
2025-04-12 16:57:07 +0900

    Add PDF section

---
+++ components/createPDFLinks.js
@@ -0,0 +1,33 @@
+import { createElem } from './utils.js';
+import { createHeading } from './createHeading.js';
+
+export function createPDFLinksSection(lesson) {
+  const pdfLinksDivElem = createElem('div', 'div-pdf-links', '');
+  const pdfLinksH2Elm = createHeading(lesson, `PDF Links`, '1', '1');
+
+  const pdfLinksTextElem = createElem('p', 'main-text', '');
+  pdfLinksTextElem.innerHTML = pdfLinksMessage;
+
+  const pdfULElem = createElem('ul', 'pdf-link-group', '');
+
+  lesson.pdfs.forEach((pdfItem) => {
+    const pdfLIElem = createElem('li', '', '');
+    const pdfLinkElem = createElem('a', 'pdf-link', '');
+    pdfLinkElem.href = `./assets/pdfs/${pdfItem.file}.pdf`;
+    pdfLinkElem.target = `_blank`;
+    pdfLinkElem.innerHTML = `${pdfItem.desc}`;
+
+    pdfLIElem.appendChild(pdfLinkElem);
+    pdfULElem.appendChild(pdfLIElem);
+  });
+  pdfLinksTextElem.appendChild(pdfULElem);
+
+  pdfLinksDivElem.appendChild(pdfLinksH2Elm);
+  pdfLinksDivElem.appendChild(pdfLinksTextElem);
+
+  return pdfLinksDivElem;
+}
+
+const pdfLinksMessage = [
+  `<b>Useful PDFs:</b>`
+]
