1724b7c91855ffa344411302dd42374451a4b3bc
nova-gavin <gavin_cronje@nova.co.jp>
2025-04-12 17:21:57 +0900

    Add Reading section

---
+++ components/createReading.js
@@ -0,0 +1,29 @@
+import { createElem } from "./utils.js";
+import { createHeading } from "./createHeading.js";
+
+export function createReadingSection(lesson, book, level) {
+  const readingDivElem = createElem('div', 'div-opening', '');
+  const readingH2Elm = createHeading(lesson, `Reading Section`, '2', '2');
+  const readingImgDiv = createElem('div', 'flex-div flex-wrap flex-justify-evenly', '');
+
+  const readingTextElem = createElem('p', 'main-text', '');
+  readingTextElem.innerHTML = readingMessage;
+
+  lesson.readingImages.forEach((readingItem) => {
+
+    const readingImgElem = createElem('img', 'image-medium', '');
+    readingImgElem.src = `./assets/${book}/${level}/${readingItem}.jpg`;
+    readingImgElem.onclick = () => { showSrcMedia(); };
+
+    readingImgDiv.appendChild(readingImgElem);
+  });
+
+  readingDivElem.appendChild(readingH2Elm);
+  readingDivElem.appendChild(readingTextElem);
+  readingDivElem.appendChild(readingImgDiv);
+  return readingDivElem;
+}
+
+const readingMessage = [
+  `<b>Aim:</b> <em>Practice reading comprehension.</em>`
+]
